e643897f0a31a4829def45714e38310e476eda65
GoparajuChandu <chandugoparajuC4@gmail.com>
2026-05-04 19:23:06 +0530

    SOD

---
+++ sumofdig.js
@@ -0,0 +1,8 @@
+let num = 5678; 
+let total = 0;
+while (num != 0) {
+    let digit = num % 10; 
+    total = total + digit;
+    num = parseInt(num / 10); 
+}
+console.log("Total of digits is: " + total);
